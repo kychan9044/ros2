8379c1af55214e13915c893eda422546fbc9869c
kychan9044 <kychan9044@naver.com>
2022-11-19 14:53:36 +0900

    fix

--- cat/setup.py
+++ cat/setup.py
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'helloworld_publisher = cat.node.helloworld_publisher:main',
             'helloworld_subscriber = cat.node.helloworld_subscriber:main',
-            'camera_publisher = cat.node.camera_subscriber:main',
+            'camera_publisher = cat.node.camera_publisher:main',
             'camera_subscriber = cat.node.camera_subscriber:main'
         ],
     },
