73a233a49651edcc4265a060d7acadc467ec12fe
kychan9044 <kychan9044@naver.com>
2022-12-02 20:25:36 +0900

    add cv2

--- test/image_predict.py
+++ test/image_predict.py
@@ -1,6 +1,7 @@
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
+import cv2
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
