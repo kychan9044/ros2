f47476de540a320ffb2bca36639235aabf831bfa
kychan9044 <kychan9044@naver.com>
2022-12-02 21:11:38 +0900

    add package

--- test/image_predict.py
+++ test/image_predict.py
@@ -3,6 +3,7 @@ from detectron2.config import get_cfg
 from detectron2 import model_zoo
 import cv2
 from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
