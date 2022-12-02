f90772622a4de0a8c33c7539186c21ca042a8034
kychan9044 <kychan9044@naver.com>
2022-12-02 20:08:34 +0900

    fix error

--- test/image_predict.py
+++ test/image_predict.py
@@ -1,4 +1,5 @@
 from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
