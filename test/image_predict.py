21479d35a0872bc1704a270c717f9067a2d53967
kychan9044 <kychan9044@naver.com>
2022-12-03 18:43:02 +0900

    add

--- test/image_predict.py
+++ test/image_predict.py
@@ -30,13 +30,15 @@ def detect_gesture():
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
     predictor = DefaultPredictor(cfg)
 
-    im = cv2.imread('./images/image3.jpeg')
+    im = cv2.imread('./images/outcome/outcome18.jpg')
     outputs = predictor(im)
-    # v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-    # out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    # cv2.imshow("image", out.get_image()[:, :, ::-1])
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow("image", out.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     print(outputs)
     print(outputs["instances"].pred_classes)
     print(outputs["instances"].pred_boxes)
+
+detect_gesture()
