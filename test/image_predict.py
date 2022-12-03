8730323df1a65c378f5b09bbed8d3821b691ec08
kychan9044 <kychan9044@naver.com>
2022-12-03 23:53:01 +0900

    fix

--- test/image_predict.py
+++ test/image_predict.py
@@ -17,13 +17,13 @@ def detect_gesture():
     # cfg.DATASETS.TRAIN = ("mdata1_train",)
     # cfg.DATASETS.TEST = ()
     # cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     # cfg.SOLVER.IMS_PER_BATCH = 2
     # cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
     # cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     # cfg.SOLVER.STEPS = []        # do not decay learning rate
     # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
 
     # os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     # trainer = DefaultTrainer(cfg) 
