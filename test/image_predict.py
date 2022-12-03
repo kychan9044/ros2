1f28b681e37670ddbde560f4563566e36cf68b33
kychan9044 <kychan9044@naver.com>
2022-12-04 01:48:59 +0900

    fix

--- test/image_predict.py
+++ test/image_predict.py
@@ -15,7 +15,8 @@ def detect_gesture():
     cfg = get_cfg()
     # cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     # cfg.DATASETS.TRAIN = ("mdata1_train",)
-    # cfg.DATASETS.TEST = ()
+    cfg.DATASETS.TEST = ()
+    test_metadata = MetadataCatalog.get("mdata1_val").set(thing_classes=["palm", "punch", "one", "two"])
     # cfg.DATALOADER.NUM_WORKERS = 2
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     # cfg.SOLVER.IMS_PER_BATCH = 2
@@ -23,7 +24,7 @@ def detect_gesture():
     # cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     # cfg.SOLVER.STEPS = []        # do not decay learning rate
     # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
 
     # os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     # trainer = DefaultTrainer(cfg) 
@@ -38,7 +39,8 @@ def detect_gesture():
 
     im = cv2.imread('./images/outcome/outcome16.jpg')
     outputs = predictor(im)
-    v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    # v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    v = Visualizer(im[:,:,::-1], metadata=test_metadata, scale=1.2)
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     cv2.imshow("image", out.get_image()[:, :, ::-1])
     cv2.waitKey(0)
