1bef43e91c627e726b56af395c3030033a40b9fb
kychan9044 <kychan9044@naver.com>
2022-12-03 17:52:08 +0900

    fix

--- cat/cat/node/detect_gesture.py
+++ cat/cat/node/detect_gesture.py
@@ -8,7 +8,7 @@ import cv2
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("mdata16_train",)
+cfg.DATASETS.TRAIN = ("mdata4_train",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
@@ -17,7 +17,7 @@ cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
 cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
 cfg.SOLVER.STEPS = []        # do not decay learning rate
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # only has one class (mount).
 
 cfg.MODEL.DEVICE='cpu'
 cfg.MODEL.WEIGHTS = "./model_final.pth"  # path to the model we just trained
