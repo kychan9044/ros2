414a649763496783a15a9f7c90c2696e77377ec6
kychan9044 <kychan9044@naver.com>
2022-12-04 12:06:43 +0900

    fix

--- cat/cat/node/detect_gesture.py
+++ cat/cat/node/detect_gesture.py
@@ -8,36 +8,22 @@ import cv2
 
 class Gesture():
     def __init__(self):
-        dataset_name = 'mdata1_val'
-        if dataset_name in DatasetCatalog.list():
-            DatasetCatalog.remove(dataset_name)
-        self.cfg = get_cfg()
-        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-        self.cfg.DATASETS.TRAIN = ("mdata1_train",)
-        self.cfg.DATASETS.TEST = ()
-        self.cfg.DATALOADER.NUM_WORKERS = 2
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-        self.cfg.SOLVER.IMS_PER_BATCH = 2
-        self.cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-        self.cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-        self.cfg.SOLVER.STEPS = []        # do not decay learning rate
-        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  # only has one class (mount).
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        cfg.DATASETS.TEST = ("mdata1_val",)
+        self.test_metadata = MetadataCatalog.get("mdata1_val").set(thing_classes=["palm", "punch", "one","two"])
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
 
-        self.cfg.MODEL.DEVICE='cpu'
-        self.cfg.MODEL.WEIGHTS = "./model_final.pth"  # path to the model we just trained
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
-        self.predictor = DefaultPredictor(self.cfg)
-
-        MetadataCatalog.get("mdata1_train").set(thing_classes=["palm", "punch", "one", "two"])
-        self.my_metadata = MetadataCatalog.get("mdata1_train")
-        MetadataCatalog.get("mdata1_val").set(thing_classes=["palm", "punch", "one", "two"])
-        self.my_metadata = MetadataCatalog.get("mdata1_val")
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
+        cfg.MODEL.DEVICE='cpu'
+        cfg.MODEL.WEIGHTS = "model_final.pth"  # path to the model we just trained
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+        self.predictor = DefaultPredictor(cfg)
         print("=================Finish Init========================")
 
     def detect_gesture(self,img,id):
         outputs = self.predictor(img)
-        v = Visualizer(img[:,:,::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1.2)
+        v = Visualizer(img[:,:,::-1], metadata=self.test_metadata, scale=1.2)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         cv2.imwrite("outcome"+str(id)+".jpg", out.get_image()[:, :, ::-1])
         # cv2.imshow("image", out.get_image()[:, :, ::-1])
